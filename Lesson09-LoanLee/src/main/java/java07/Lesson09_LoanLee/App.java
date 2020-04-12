3e6efc18b725633de4a6cfc6e6de04737e884124
loanbabycute <loanlee2198@gmail.com>
2020-04-12 09:22:32 +0700

    Lesson09 LoanLee

---
+++ Lesson09-LoanLee/src/main/java/java07/Lesson09_LoanLee/App.java
@@ -0,0 +1,13 @@
+package java07.Lesson09_LoanLee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
