f237191627637978596d486f5292dfb800d5c15c
loanbabycute <loanlee2198@gmail.com>
2020-04-12 08:55:19 +0700

    Lesson08-Loanlee submit

---
+++ Lesson08-LoanLee/src/main/java/java07/Lesson08_LoanLee/App.java
@@ -0,0 +1,13 @@
+package java07.Lesson08_LoanLee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Lesson08-LoanLee/src/main/java/java07/Lesson08_LoanLee/Ex01.java
@@ -0,0 +1,5 @@
+package java07.Lesson08_LoanLee;
+
+public class Ex01 {
+
+}
